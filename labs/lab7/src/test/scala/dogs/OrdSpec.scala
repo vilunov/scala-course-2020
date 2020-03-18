473a18ca0fa0284ab8738d60afbb26b64b0cbed8
Anastasia Reprintseva <a.reprintseva@tinkoff.ru>
2020-03-18 20:37:57 +0300

    [Lab 7] Extend tests to include property laws of PartialOrd and Ord

--- labs/lab7/src/test/scala/dogs/OrdSpec.scala
+++ labs/lab7/src/test/scala/dogs/OrdSpec.scala
@@ -1,9 +1,17 @@
 package dogs
 
-object OrdIntSpec extends EqualityProperties[Int]("intOrd") {
+object EqIntSpec extends EqualityProperties[Int]("intEq") {
   override def instance: Equality[Int] = Ord[Int]
 }
 
-object OrdLongSpec extends EqualityProperties[Long]("longOrd") {
+object EqLongSpec extends EqualityProperties[Long]("longEq") {
   override def instance: Equality[Long] = Ord[Long]
+}
+
+object OrdIntSpec extends OrdProperties[Int]("intOrd") {
+  override def instance: Ord[Int] = Ord[Int]
+}
+
+object OrdLongSpec extends OrdProperties[Long]("longOrd") {
+  override def instance: Ord[Long] = Ord[Long]
 }
--- labs/lab7/src/test/scala/dogs/Properties.scala
+++ labs/lab7/src/test/scala/dogs/Properties.scala
@@ -22,3 +22,77 @@ abstract class PartialEqualityProperties[T: Arbitrary](name: String) extends Pro
     !(instance.equal(a, b) && instance.equal(b, c)) || instance.equal(a, c)
   }
 }
+
+abstract class OrdProperties[T: Arbitrary](name: String) extends Properties(name) {
+  def instance: Ord[T]
+
+  property("total order is reflexive") = forAll { a: T =>
+      instance.compare(a, a) != OrdResult.Greater
+    }
+
+  property("total order is antisymmetric") = forAll { (a: T, b: T) =>
+      val lessOrEqual = instance.compare(a, b) != OrdResult.Greater &&
+        instance.compare(b, a) != OrdResult.Greater
+      val equal = instance.equal(a, b)
+
+      !lessOrEqual || equal
+    }
+
+  property("total order is transitive") = forAll { (a: T, b: T, c: T) =>
+      val lessOrEqual = instance.compare(a, b) != OrdResult.Greater &&
+        instance.compare(b, c) != OrdResult.Greater
+
+      val notGreater = instance.compare(a, c) != OrdResult.Greater
+
+      !lessOrEqual || notGreater
+  }
+}
+
+abstract class PartialOrdProperties[T: Arbitrary](name: String) extends Properties(name) {
+  def instance: PartialOrd[T]
+
+  property("total order is reflexive") = forAll { a: T =>
+    instance.partialCompare(a, a) match {
+      case Some(res) => res != OrdResult.Greater
+      case _ => true
+    }
+  }
+
+  property("total order is antisymmetric") = forAll { (a: T, b: T) =>
+    val aLessOrEqualB = instance.partialCompare(a, b) match {
+      case Some(res) => res != OrdResult.Greater
+      case _ => true
+    }
+
+    val bLessOrEqualA = instance.partialCompare(b, a) match {
+      case Some(res) => res != OrdResult.Greater
+      case _ => true
+    }
+
+    val equal = instance.partialCompare(a, b) match {
+      case Some(res) => res == OrdResult.Equal
+      case _ => false
+    }
+
+    !(aLessOrEqualB && bLessOrEqualA) || equal
+  }
+
+  property("total order is transitive") = forAll { (a: T, b: T, c: T) =>
+    val aLessOrEqualB = instance.partialCompare(a, b) match {
+      case Some(res) => res != OrdResult.Greater
+      case _ => true
+    }
+
+    val bLessOrEqualС = instance.partialCompare(b, c) match {
+      case Some(res) => res != OrdResult.Greater
+      case _ => true
+    }
+
+    val notGreater = instance.partialCompare(a, c) match {
+      case Some(res) => res != OrdResult.Greater
+      case _ => false
+    }
+
+    !(aLessOrEqualB && bLessOrEqualС) || notGreater
+  }
+}
