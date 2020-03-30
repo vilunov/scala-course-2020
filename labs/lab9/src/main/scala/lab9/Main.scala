d1c89ca86a18a22abee2785f94cc6b392f6a7f1d
Anastasia Reprintseva <a.reprintseva@tinkoff.ru>
2020-03-30 17:13:07 +0300

    Implementation of instances.

--- labs/lab9/src/main/scala/lab9/Main.scala
+++ labs/lab9/src/main/scala/lab9/Main.scala
@@ -8,4 +8,8 @@ class Main extends App {
   val option1 = Option(123)
 //  val option2 = Functor[Option].map(option1)(_.toString) // option2: Option[String] = Some(123)
 
+  val opt1 = Monad[Option].pure(3) // opt1: Option[Int] = Some(3)
+//  val opt2 = Monad[Option].flatMap(opt1)(a => Some(a + 2)) // opt2: Option[Int] = Some(5)
+//  val opt3 = Monad[Option].map(opt2)(a => 100 * a)
+
 }
--- labs/lab9/src/main/scala/lab9/Monad.scala
+++ labs/lab9/src/main/scala/lab9/Monad.scala
@@ -1,5 +1,7 @@
 package lab9
 
+import scala.concurrent.{ExecutionContext, Future}
+
 trait Monad[F[+_]] extends Functor[F] {
   def pure[T](v: T): F[T]
   def flatMap[A, B](f: A => F[B], v: F[A]): F[B]
@@ -10,4 +12,48 @@ trait Monad[F[+_]] extends Functor[F] {
 
 object Monad {
   def apply[F[+_]](implicit monad: Monad[F]): Monad[F] = monad
+
+  implicit def optionMonad: Monad[Option] = new Monad[Option] {
+    override def pure[T](v: T): Option[T] = Some(v)
+
+    override def flatMap[A, B](f: A => Option[B], v: Option[A]): Option[B] = v.flatMap(f)
+  }
+
+  implicit def eitherMonad[K]: Monad[Either[K, +*]] = new Monad[Either[K, +*]] {
+    override def pure[T](v: T): Either[K, T] = Right(v)
+
+    override def flatMap[A, B](f: A => Either[K, B], v: Either[K, A]): Either[K, B] = v match {
+      case Left(v) => Left(v)
+      case Right(v) => f(v)
+    }
+  }
+
+  implicit def vectorMonad: Monad[Vector] = new Monad[Vector] {
+    override def pure[T](v: T): Vector[T] = Vector(v)
+
+    override def flatMap[A, B](f: A => Vector[B], v: Vector[A]): Vector[B] = v.flatMap(f)
+  }
+
+  implicit def listMonad: Monad[List] = new Monad[List] {
+    override def pure[T](v: T): List[T] = List(v)
+
+    override def flatMap[A, B](f: A => List[B], v: List[A]): List[B] = v.flatMap(f)
+  }
+
+  implicit def unaryFunctionMonad[K]: Monad[K => +*] = new Monad[Function[K, +*]] {
+    override def pure[T](v: T): Function[K, T] = {
+      _: K => v
+    }
+
+    override def flatMap[A, B](f: A => Function[K, B], v: Function[K, A]): Function[K, B] = {
+      param: K => f(v(param))(param)
+    }
+  }
+
+  implicit def futureMonad(implicit context: ExecutionContext): Monad[Future] = new Monad[Future] {
+    override def pure[T](v: T): Future[T] = Future(v)
+
+    override def flatMap[A, B](f: A => Future[B], v: Future[A]): Future[B] = v.flatMap(f)
+  }
+
 }
